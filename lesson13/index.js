b33b92f03f529991f76957c7d8c7ceddc826ae46
AyubovAbdulaziz <abdulazizayubov@gmail.com>
2026-04-20 09:03:50 +0500

    return homework

---
+++ lesson13/index.js
@@ -0,0 +1,152 @@
+
+// let a = 10
+// let b = 20
+// function yigindi(raqam1, raqam2) {
+//     let summa = raqam1 + raqam2;
+//     return summa;
+// }
+// console.log(yigindi(a, b));
+
+// console.log("-----------------------------------------");
+
+// let c = 20
+// let d = 10
+// function e(f, j) {
+//     let h = f * j;
+//     return h;
+// }
+// console.log(e(c, d));
+
+// console.log("-----------------------------------------");
+
+// let o = 70
+// function k(bb) {
+//     let dd = bb % 2 === 0
+//     return dd;
+// }
+// console.log(k(o));
+
+// console.log("-----------------------------------------");
+
+// let l = 70
+// function v(zz) {
+//     let gg = zz % 2 === 1
+//     return gg;
+// }
+// console.log(v(l));
+
+// console.log("-----------------------------------------");
+
+// let aa = 20
+// let ww = 10
+// function n(xx, mm) {
+//     let hh = xx - mm;
+//     return hh;
+// }
+// console.log(n(aa, ww));
+
+// console.log("-----------------------------------------");
+
+// let aaa = 20
+// let w = 10
+// function n(x, m) {
+//     let hhh = x * m;
+//     return hhh;
+// }
+// console.log(n(aaa, w));
+
+// console.log("-----------------------------------------");
+
+// let aaaa = 20
+// let www = 10
+// function yi(xxxx, mmm) {
+//     let summa1 = xxxx / mmm;
+//     return summa1;
+// }
+// console.log(yi(aaaa, www));
+
+// console.log("-----------------------------------------");
+
+// let a1 = 20
+// let w1 = 10
+// function yi1(x1, m1) {
+//     let summa2 = x1 > m1;
+//     return summa2;
+// }
+// console.log(yi1(a1, w1));
+
+// console.log("-----------------------------------------");
+
+// let a2 = 20
+// let w2 = 10
+// function yi2(x2, m2) {
+//     let summa3 = x2 < m2;
+//     return summa3;
+// }
+// console.log(yi2(a2, w2));
+
+// console.log("-----------------------------------------");
+
+// let a3 = 20
+// let w3 = 10
+// function yi3(x3, m3) {
+//     let summa4 = x3 == m3;
+//     return summa4;
+// }
+// console.log(yi3(a3, w3));
+
+// console.log("-----------------------------------------");
+
+// let l1 = 70
+// function v1(zz1) {
+//     let gg1 = zz1 % 7 || 5 
+//     return gg1;
+// }
+// console.log(v1(l1));
+
+// console.log("-----------------------------------------");
+
+// let l2 = 70
+// function v2(zz2) {
+//     let gg2 = zz2 ** l2
+//     return gg2;
+// }
+// console.log(v2(l2));
+
+// console.log("-----------------------------------------");
+
+// let a4 = 20
+// let w4 = 10
+// function yi4(x4, m4) {
+//     let summa5 = x4 <= m4;
+//     return summa5;
+// }
+// console.log(yi4(a4, w4));
+
+// console.log("-----------------------------------------");
+
+// let a5 = 20
+// let w5 = 10
+// function yi5(x5, m5) {
+//     let summa6 = x5 >= m5;
+//     return summa6;
+// }
+// console.log(yi5(a5, w5));
+
+// console.log("-----------------------------------------");
+
+// let l3 = 70
+// function v3(zz3) {
+//     let gg3 = zz3 *= l3
+//     return gg3;
+// }
+// console.log(v3(l3));
+
+// console.log("-----------------------------------------");
+
+// let l4 = 70
+// function v4(zz4) {
+//     let gg4 = zz4 /= l4
+//     return gg4;
+// }
+// console.log(v4(l4));
