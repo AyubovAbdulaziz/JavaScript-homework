5425308c41daf70a78630b201e0770b28acef1ad
AyubovAbdulaziz <abdulazizayubov@gmail.com>
2026-04-23 22:05:29 +0500

    object homework

---
+++ lesson15/index.js
@@ -0,0 +1,197 @@
+// 10
+let obj = {
+    ism: "Abdulaziz",
+    familiya: "Ayubov",
+    yosh: 13,
+    isGoSchool: true,
+};
+
+console.log(obj.familiya);
+console.log(obj.ism);
+console.log(obj.yosh);
+console.log(obj.isGoSchool);
+
+console.log(obj["familiya"]);
+console.log(obj["ism"]);
+console.log(obj["yosh"]);
+console.log(obj["isGoSchool"]);
+
+console.log("/////////////////////////////////////////////////");
+
+// 2
+let a = {
+    ism: "Abdulloh",
+    familiya: "Mamadaliyev",
+    yosh: 13,
+    isGoSchool: true,
+};
+
+console.log(a.familiya);
+console.log(a.ism);
+console.log(a.yosh);
+console.log(a.isGoSchool);
+
+console.log(a["familiya"]);
+console.log(a["ism"]);
+console.log(a["yosh"]);
+console.log(a["isGoSchool"]);
+
+console.log("/////////////////////////////////////////////////");
+
+// 3
+let b = {
+    ism: "Asrorxo'ja",
+    familiya: "Rustamov",
+    yosh: 13,
+    isGoSchool: true,
+};
+
+console.log(b.familiya);
+console.log(b.ism);
+console.log(b.yosh);
+console.log(b.isGoSchool);
+
+console.log(b["familiya"]);
+console.log(b["ism"]);
+console.log(b["yosh"]);
+console.log(b["isGoSchool"]);
+
+console.log("/////////////////////////////////////////////////");
+
+// 4
+let d = {
+    ism: "Otabek",
+    familiya: "Axmadov",
+    yosh: 13,
+    isGoSchool: true,
+};
+
+console.log(d.familiya);
+console.log(d.ism);
+console.log(d.yosh);
+console.log(d.isGoSchool);
+
+console.log(d["familiya"]);
+console.log(d["ism"]);
+console.log(d["yosh"]);
+console.log(d["isGoSchool"]);
+
+console.log("/////////////////////////////////////////////////");
+
+// 5
+let c = {
+    ism: "Muhammad",
+    familiya: "Hamidullayev",
+    yosh: 13,
+    isGoSchool: true,
+};
+
+console.log(c.familiya);
+console.log(c.ism);
+console.log(c.yosh);
+console.log(c.isGoSchool);
+
+console.log(c["familiya"]);
+console.log(c["ism"]);
+console.log(c["yosh"]);
+console.log(c["isGoSchool"]);
+
+console.log("/////////////////////////////////////////////////");
+
+// 6
+let e = {
+ism: "Azizbek",
+    familiya: "Nishanov",
+    yosh: 13,
+    isGoSchool: true,
+};
+
+console.log(e.familiya);
+console.log(e.ism);
+console.log(e.yosh);
+console.log(e.isGoSchool);
+
+console.log(e["familiya"]);
+console.log(e["ism"]);
+console.log(e["yosh"]);
+console.log(e["isGoSchool"]);
+
+console.log("/////////////////////////////////////////////////");
+
+// 7
+let f = {
+ism: "Musavvir",
+    familiya: "Sadriddinov",
+    yosh: 13,
+    isGoSchool: true,
+};
+
+console.log(f.familiya);
+console.log(f.ism);
+console.log(f.yosh);
+console.log(f.isGoSchool);
+
+console.log(f["familiya"]);
+console.log(f["ism"]);
+console.log(f["yosh"]);
+console.log(f["isGoSchool"]);
+
+console.log("/////////////////////////////////////////////////");
+
+// 8
+let g = {
+ism: "Muhammad",
+    familiya: "Salohiddinov",
+    yosh: 13,
+    isGoSchool: true,
+};
+
+console.log(g.familiya);
+console.log(g.ism);
+console.log(g.yosh);
+console.log(g.isGoSchool);
+
+console.log(g["familiya"]);
+console.log(g["ism"]);
+console.log(g["yosh"]);
+console.log(g["isGoSchool"]);
+
+console.log("/////////////////////////////////////////////////");
+
+// 9
+let h = {
+ism: "Muboshsher",
+    familiya: "Sodiqov",
+    yosh: 13,
+    isGoSchool: true,
+};
+
+console.log(h.familiya);
+console.log(h.ism);
+console.log(h.yosh);
+console.log(h.isGoSchool);
+
+console.log(h["familiya"]);
+console.log(h["ism"]);
+console.log(h["yosh"]);
+console.log(h["isGoSchool"]);
+
+console.log("/////////////////////////////////////////////////");
+
+// 10
+let j = {
+ism: "Isroil",
+    familiya: "Jurayev",
+    yosh: 13,
+    isGoSchool: true,
+};
+
+console.log(j.familiya);
+console.log(j.ism);
+console.log(j.yosh);
+console.log(j.isGoSchool);
+
+console.log(j["familiya"]);
+console.log(j["ism"]);
+console.log(j["yosh"]);
+console.log(j["isGoSchool"]);
